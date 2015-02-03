39f04ec47a32e36e99c2173451309d87cbe49f86
Petter Salminen <petsal@kth.se>
2015-02-03 09:50:07 +0100

    SOLVED PROBLEM 1

--- solver.c
+++ solver.c
@@ -2,16 +2,37 @@
 
 void read(int * x);
 void output(int problem, int answer);
-
+void solve(int * answer); // main solve clauses
 
 int main()
 {
   int x;
 
-  read(&x);
-  
+  //read(&x);
+  solve(&x);
   output(0,x);
   return 0;
+}
+
+void solve(int * answer)
+{
+  int multiples [1000];
+
+  
+  int i,j;
+  for(i=0,j=0; i<1000; i++){
+    multiples[i] = 0; // zero init array
+    if(0 == i%3 | 0 == i%5){
+      multiples[j] = i;
+      j++;
+    }
+  }
+
+  int sum;
+  for(i=0, sum=0; i<1000; i++){
+    sum+=multiples[i];
+  }
+  *answer = sum; // write answer
 }
 
 /* in case I want a program to take input */
