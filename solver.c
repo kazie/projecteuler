30bce9d36c5aeb52ad02802e773a4c943a93d8a4
Petter Salminen <petsal@kth.se>
2015-02-03 11:18:22 +0100

    SOLVED PROBLEM 4

--- solver.c
+++ solver.c
@@ -1,50 +1,57 @@
 #include <stdio.h> /* IO stuff */
 #include <stdint.h> /* Integers */
-#include <math.h> /* sqrt and stuff */
+//#include <math.h> /* sqrt and stuff */
 //#define int int64_t
 
 void read(int * x);
-void output(int problem, int64_t answer);
-void solve(int64_t * answer); // main solve clauses
+void output(int problem, int answer);
+void solve(int * answer); // main solve clauses
 
 int main()
 {
-  int64_t x;
+  int x;
 
   //read(&x);
+  //x = isPalindrome(x);
   solve(&x);
-  output(3,x);
+  output(4,x);
   return 0;
 }
 
-void solve(int64_t * answer)
+void solve(int * answer)
 {
-  int64_t n = 600851475143, i;
-
-  //Case, devides by 2
-  while(0 == n%2)
-    {
-      printf("%d, ", 2);
-      n = n/2;
-    }
-
-  for (i = 3; i <= sqrt(n); i = i+2)
-    {
-
-      while(n%i == 0)
-        {
-          printf("%lu, ", i);
-          n = n/i;
-        }
-      
-    }
-
-  // If last number is prime
-  if(n > 2){
-    printf("%lu\n", n);
-  }
-    
+  int i=999, j=999, largestPalin=0;
   
+  for(i=999;i>100;i--){
+    for(j=999;j>100;j--)
+      {
+        int mul = j*i;
+        if(1==isPalindrome(mul) && mul>largestPalin){
+          largestPalin = mul;
+        }
+      }
+  }
+  
+  *answer = largestPalin;
+  
+}
+
+// 1 = TRUE, 0 = FALSE.
+int isPalindrome(int x){
+ int n = x;
+ int rev = 0;
+ while (x > 0)
+ {
+      int dig = x % 10;
+      rev = rev * 10 + dig;
+      x = x / 10;
+ }
+
+ if(n == rev){
+   return 1;
+ } else{
+   return 0;
+ }
 }
 
 /* in case I want a program to take input */
@@ -55,7 +62,7 @@ void read(int * x)
 }
 
 /* Template output for answers */
-void output(int problem, int64_t answer)
+void output(int problem, int answer)
 {
-  printf( "The solution for problem %d is %lu \n" , problem, answer );
+  printf( "The solution for problem %d is %d \n" , problem, answer );
 }
